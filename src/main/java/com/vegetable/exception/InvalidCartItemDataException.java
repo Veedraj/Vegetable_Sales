f0311144b3b48612bd4a7823ffdaa0661e12bf17
nikharjain16 <Nikharjain27@gmail.com>
2023-04-06 15:35:16 +0530

    All updated

---
+++ src/main/java/com/vegetable/exception/InvalidCartItemDataException.java
@@ -0,0 +1,9 @@
+package com.vegetable.exception;
+
+public class InvalidCartItemDataException extends Exception{
+	
+	public InvalidCartItemDataException(String message) {
+		super(message);
+	}
+
+}
