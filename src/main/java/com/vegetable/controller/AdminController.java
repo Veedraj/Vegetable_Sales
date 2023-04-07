7a7b36cee53c24acbb733ad71a13464d59172405
nikharjain16 <Nikharjain27@gmail.com>
2023-04-07 12:28:22 +0530

    Admin Updated Code

--- src/main/java/com/vegetable/controller/AdminController.java
+++ src/main/java/com/vegetable/controller/AdminController.java
@@ -15,10 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vegetable.entity.Customer;
 import com.vegetable.entity.Order;
+import com.vegetable.entity.Payment;
 import com.vegetable.entity.Product;
 import com.vegetable.exception.CustomerNotFoundException;
 import com.vegetable.exception.InvalidProductDetailsException;
 import com.vegetable.exception.OrderNotFoundException;
+import com.vegetable.exception.PaymentNotFoundException;
 import com.vegetable.exception.ProductNotFoundException;
 import com.vegetable.service.AdminService;
 import com.vegetable.service.CustomerService;
@@ -49,9 +51,9 @@ public class AdminController {
 		return adminService.allProducts();
 	}
 	
-	@PutMapping("update/products")
-	public ResponseEntity<Product> updateproduct(@RequestBody Product product) throws InvalidProductDetailsException{
-		return new ResponseEntity<Product>(adminService.updateProduct(product), HttpStatus.OK);
+	@PutMapping("update/products/{productId}")
+	public ResponseEntity<Product> updateproduct(@RequestBody Product productId) throws InvalidProductDetailsException{
+		return new ResponseEntity<Product>(adminService.updateProductById(productId), HttpStatus.OK);
 	}
 	
 	@DeleteMapping("delete/products/{productId}")
@@ -65,4 +67,35 @@ public class AdminController {
 	{
 		return adminService.allCustomers();
 	}
+	
+	
+	@PutMapping("update/customers/{customerId}")
+	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customerId){
+		return new ResponseEntity<Customer>(adminService.updateCustomerById(customerId), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("delete/customer/{customerId}")
+	public ResponseEntity<List<Customer>> deleteCustomerById(@PathVariable("customerId") Long customerId) throws CustomerNotFoundException{
+		List<Customer> customerList = adminService.deleteCustomerById(customerId);
+		return new ResponseEntity<List<Customer>>(customerList, HttpStatus.OK);
+	}
+	
+	@GetMapping("/view/payments")
+	public List<Payment> allPayments() throws PaymentNotFoundException
+	{
+		return adminService.allPayments();
+	}
+	
+	
+	@PutMapping("update/payments/{paymentId}")
+	public ResponseEntity<Payment> updatePayments(@RequestBody Payment paymentId){
+		return new ResponseEntity<Payment>(adminService.updatePaymentById(paymentId), HttpStatus.OK);
+	}
+	
+	@DeleteMapping("delete/payments/{paymentId}")
+	public ResponseEntity<List<Payment>> deletePaymentById(@PathVariable("paymentId") Long paymentId) throws PaymentNotFoundException{
+		List<Payment> paymentList = adminService.deletePaymentById(paymentId);
+		return new ResponseEntity<List<Payment>>(paymentList, HttpStatus.OK);
+	}
+	
 }
--- src/main/java/com/vegetable/service/AdminService.java
+++ src/main/java/com/vegetable/service/AdminService.java
@@ -2,12 +2,16 @@ package com.vegetable.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.vegetable.entity.Customer;
 import com.vegetable.entity.Order;
+import com.vegetable.entity.Payment;
 import com.vegetable.entity.Product;
 import com.vegetable.exception.CustomerNotFoundException;
 import com.vegetable.exception.InvalidProductDetailsException;
 import com.vegetable.exception.OrderNotFoundException;
+import com.vegetable.exception.PaymentNotFoundException;
 import com.vegetable.exception.ProductNotFoundException;
 
 public interface AdminService {
@@ -18,12 +22,19 @@ public interface AdminService {
 	
 	public List<Product> allProducts() throws ProductNotFoundException;
 
-	public Product updateProduct(Product product) throws InvalidProductDetailsException;
+	public Product updateProductById(Product product) throws InvalidProductDetailsException;
 
 	public List<Product> deleteProductById(Long productId)throws ProductNotFoundException;
 	
 	public List<Customer> allCustomers() throws CustomerNotFoundException;
 	
-	public Customer updateCustomer(Customer customer);
+	public Customer updateCustomerById(Customer customer);
 	
+	public List<Customer> deleteCustomerById(Long customerId);
+	
+	public List<Payment> allPayments() throws PaymentNotFoundException;
+	
+	public Payment updatePaymentById(Payment paymentId);
+	
+	public List<Payment> deletePaymentById(Long paymentId);
 }
